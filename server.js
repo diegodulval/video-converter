6c92d8544212bf0e5aff01619f2633921d37df24
Diego Dulval de Freitas <diegodifreitas@gmail.com>
2018-07-31 18:17:18 -0300

    Create enpoint for upload

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const PORT = config.get("port");
 const multer = require("multer");
 const cookieParser = require("cookie-parser");
 const cookieMiddleware = require("./middleware/userCookie");
+const fs = require("fs");
+const path = require("path");
 
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
@@ -26,6 +28,39 @@ const upload = multer({ storage: storage });
 
 app.get("*", (req, res) => {
   res.render("app");
+});
+
+app.post("/upload", upload.single("file"), (req, res) => {
+  console.log(req);
+
+  if (req.file) {
+    let video = req.file;
+    console.log("Ta aqui");
+    let upload_path = path.join(__dirname, "/uploads/");
+
+    fs.exists(upload_path, exists => {
+      if (!exists) {
+        fs.mkdir(upload_path, function(err) {
+          if (err) {
+            return console.error(err);
+          }
+          res.json({
+            uploaded: true,
+            path: video.filename
+          });
+        });
+      } else {
+        res.json({
+          uploaded: true,
+          path: video.filename
+        });
+      }
+    });
+  } else {
+    res.json({
+      uploaded: false
+    });
+  }
 });
 
 server.listen(PORT, () => console.log("Server running on Port: " + PORT));
