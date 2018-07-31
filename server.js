73a09b3680fca3b0ae28b27ed0439b1c0ae71996
Diego Dulval de Freitas <diegodifreitas@gmail.com>
2018-07-31 17:55:04 -0300

    Configured multer

--- server.js
+++ server.js
@@ -4,16 +4,25 @@ const server = require("http").Server(app);
 const io = require("socket.io")(server);
 const config = require("config");
 const PORT = config.get("port");
-
+const multer = require("multer");
 const cookieParser = require("cookie-parser");
 const cookieMiddleware = require("./middleware/userCookie");
-
-app.use(cookieParser());
-app.use(cookieMiddleware());
 
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
 app.use("/", express.static(__dirname + "/public"));
+app.use(cookieParser());
+app.use(cookieMiddleware());
+
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, path.resolve(__dirname, "uploads"));
+  },
+  filename: function(req, file, cb) {
+    cb(null, Date.now() + "_" + file.originalname);
+  }
+});
+const upload = multer({ storage: storage });
 
 app.get("*", (req, res) => {
   res.render("app");
