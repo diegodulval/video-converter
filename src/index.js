bb502de0907bf4f5fd8d3fa2dfc093909ff61fea
Diego Dulval de Freitas <diegodifreitas@gmail.com>
2018-08-01 09:07:00 -0300

    Added mini.css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import "mini.css";
+import "./index.scss";
 
 ReactDOM.render(<App />, document.getElementById("root"));
